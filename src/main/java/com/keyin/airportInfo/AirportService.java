0938a1b6e4553d7e38e902ebb6ffb8315b3cfa64
ashley-colbert <136830986+ashley-colbert@users.noreply.github.com>
2024-06-18 12:52:44 -0230

    airport service and controller working with postman

--- src/main/java/com/keyin/airportInfo/AirportService.java
+++ src/main/java/com/keyin/airportInfo/AirportService.java
@@ -1,4 +1,56 @@
 package com.keyin.airportInfo;
 
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class AirportService {
+    private Map<Integer, Airports> airportMap = new HashMap<Integer, Airports>();
+
+    public Airports getAirport(Integer index) {
+        return airportMap.get(index);
+    }
+
+    public Airports createAirport(Airports newAirport) {
+        airportMap.put(airportMap.size() +1, newAirport);
+        return newAirport;
+    }
+
+    public List<Airports> getAllAirports() {
+        return List.copyOf(airportMap.values());
+    }
+
+    public Airports updateAirport(Integer index, Airports updatedAirport) {
+        Airports airportToUpdate = airportMap.get(index);
+
+        airportToUpdate.setId(updatedAirport.getId());
+        airportToUpdate.setName(updatedAirport.getName());
+        airportToUpdate.setCode(updatedAirport.getCode());
+
+        airportMap.put(index, airportToUpdate);
+        return airportToUpdate;
+    }
+
+    public void deleteAirport(Integer index) {
+        airportMap.remove(index);
+    }
+
+    public List<Airports> findAirportByIdNameCode(String id, String name, String code) {
+        List<Airports> airportsFound = new ArrayList<Airports>();
+
+        for (Airports airports : airportMap.values()) {
+            if (airports.getId().equalsIgnoreCase(id) &&
+                airports.getName().equalsIgnoreCase(name) &&
+                airports.getCode().equalsIgnoreCase(code)) {
+                airportsFound.add(airports);
+            }
+        }
+        return airportsFound;
+    }
+
 }
